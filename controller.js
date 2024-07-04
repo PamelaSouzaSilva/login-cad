234afdde1c34ec59c8a4b4ded1e85162469c8c5f
Pamela <p.silva39@alunoescolassociais.org.br>
2024-07-04 08:34:26 -0300

    implementação do codigo JavaScript

--- controller.js
+++ controller.js
@@ -10,3 +10,18 @@ function acessar(){
       window.location.href = 'cadastro.html';
     }
 }
+
+// FUNÇÃO DE CRIAÇÃO DE ARRAY PARA ARMAZENAMENTO DE NOMES
+
+var dadosLista = [];
+
+function salvarUser(){
+    let nomeUser = document.getElementById('nomeUser').value;
+
+    if(nomeUser){
+
+    } else{
+        alert("Favor, informar um nome");
+    }
+
+}
